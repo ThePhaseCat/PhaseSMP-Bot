8c9c494a599fdf4c6447db98d4d3dfb6655bbd1c
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-04-12 17:15:12 -0600

    Created fox.js
    First command to pull from internet using node-fetch
    
    More should come soon (I hope)

---
+++ commands/fox.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('fox')
+        .setDescription('please work'),
+  
+  async execute(interaction) {
+      const res = await fetch('https://randomfox.ca/floof/');
+      const img = (await res.json()).image;
+      const embed = new MessageEmbed()
+        .setTitle('🦊  What does the fox say?  🦊')
+        .setImage(img)
+   await interaction.reply({ embeds: [embed]});
+    }
+  };
