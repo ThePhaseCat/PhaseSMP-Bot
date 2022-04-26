85125193ce17ec4157404cf91fbe1a4a6559a5d3
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-04-26 14:36:44 -0600

    Update prmap.js

--- commands/prmap.js
+++ commands/prmap.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('prmap')
         .setDescription('Provides image for the PhaseRail Map'),
   async execute(interaction) {
-		return interaction.reply(`This will be updated soon with a two maps; a map that contains line paths on top of a geographic map and a map containing just the stations along a route.`);
+		return interaction.reply(`This will be updated soon with a two maps; a map that contains line paths on top of a geographic map and a map containing just the stations along a route.`, ephemeral: true );
 	},
 };
