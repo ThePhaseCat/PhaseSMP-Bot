0330b0a275f9ad3859846bce15a5488d1d7a0511
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-04-26 14:37:07 -0600

    Update prinfo.js

--- commands/prinfo.js
+++ commands/prinfo.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+// at the top of your file
+const { MessageEmbed } = require('discord.js');
+
+// inside a command, event listener, etc.
+const railEmbed = new MessageEmbed()
+	.setColor('#800080')
+	.setTitle('PhaseRail Information')
+	.addFields(
+		{ name: 'General Description', value: 'PhaseRail is a railway network that will eventually span most of the inhabited regions of the PhaseSMP' },
+    { name: 'Line Count', value: 'Currently, there is only one line on the PhaseRail Network', inline: true },
+    { name: 'Line 1', value: '600 Blocks going from Phase-Cave Station to Spawn Station via Phase-Mall. **There are plans for an extension later down the line**', inline: true },
+	);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('prinfo')
         .setDescription('Provides details on PhaseRail'),
   async execute(interaction) {
-		return interaction.reply(`Details on PhaseRail will arrive once the first line opens...`);
+		await interaction.reply({ embeds: [railEmbed], ephemeral: true });
 	},
 };
