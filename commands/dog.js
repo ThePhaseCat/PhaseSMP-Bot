cd2620e3ef4be885b4d6089df67e0214ce6a44cc
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-04-29 09:06:11 -0600

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -6,7 +6,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dog')
-        .setDescription('Finds a random duck for your viewing pleasure'),
+        .setDescription('Finds a random dog for your viewing pleasure'),
   
   async execute(interaction) {
       const res = await fetch('https://dog.ceo/api/breeds/image/random');
