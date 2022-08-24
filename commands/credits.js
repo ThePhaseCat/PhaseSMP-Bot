f0e88165cf16bd1e85724e12a2a3851f8c865125
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-08-24 13:18:43 -0600

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('credits')
-        .setDescription('Sends a message detailing who made this bot'),
+        .setDescription('Who made the bot?'),
   async execute(interaction) {
-		return interaction.reply(`This bot was made by ThePhaseCat! :D`);
+		return interaction.reply(`This bot was made by ThePhaseCat! Meow!`);
 	},
 };
