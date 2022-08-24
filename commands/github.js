1e977eb68a175d3bde27caa12f899968cf4a0a5a
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-08-24 13:19:41 -0600

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('github')
-        .setDescription('Sends link to the github page for this bot'),
+        .setDescription('Sends link to Github (In case you could not look at the bio of the bot'),
   async execute(interaction) {
 		return interaction.reply(`Here's the link to this bot's github: https://github.com/ThePhaseCat/PhaseSMP-Bot`);
 	},
