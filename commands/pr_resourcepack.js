8607035fcf6a0f27cad53bb906d2d388e6c12ba8
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2022-08-24 13:23:15 -0600

    Update pr_resourcepack.js

--- commands/pr_resourcepack.js
+++ commands/pr_resourcepack.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('resourcepack')
-        .setDescription('Sends link to the github/download page for the PhaseSMP Rail Network Pack for use in sessions other than the PhaseSMP'),
+        .setDescription('Sends link to the github/download page for the PhaseSMP Rail Network Pack if you need to use it in your own worlds!'),
   async execute(interaction) {
-		return interaction.reply(`Here's the link to the resource pack for the PhaseSMP Rail Network Pack: https://github.com/ThePhaseCat/PhaseSMP-Rail-Network-Pack`);
+		return interaction.reply(`Here's the link to the resource pack for the PhaseSMP Rail Network Pack (Please note that the resource pack is auto-downloaded via the server, so there's no need to use this in the server): https://github.com/ThePhaseCat/PhaseSMP-Rail-Network-Pack`);
 	},
 };
